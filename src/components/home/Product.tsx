034079747aad0e5881ed049b780c01b7ee7782d1
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2022-12-19 23:17:30 +0700

    adjust UI

--- src/components/home/Product.tsx
+++ src/components/home/Product.tsx
@@ -65,7 +65,7 @@ function Product({
       <p className="absolute top-2 right-2 text-xs italic text-gray-400">
         {category_id}
       </p>
-      <div className="relative h-[90px] aspect-square overflow-hidden">
+      <div className="relative h-full max-h-[200px] aspect-square overflow-hidden">
         <FallbackImage
           quality={100}
           layout="fill"
--- src/pages/product/[slug].tsx
+++ src/pages/product/[slug].tsx
@@ -7,6 +7,8 @@ import Seo from "@/components/Seo";
 import CommentsSection from "@/components/product/CommentsSection";
 import { Basket, addToBasket } from "@/redux/slices/basketSlice";
 import fetchDetailProduct from "@/services/products/fetch-detail.service";
+import { Fragment } from "react";
+import Button from "@/components/core/Button";
 
 const DetailProduct = () => {
   const router = useRouter();
@@ -25,6 +27,8 @@ const DetailProduct = () => {
       refetchOnWindowFocus: false,
     }
   );
+
+  const rating = products?.data?.rating as number;
 
   return (
     <main>
@@ -46,29 +50,41 @@ const DetailProduct = () => {
               </h1>
               <div className="flex mb-4">
                 <span className="flex items-center">
-                  <svg
-                    fill="currentColor"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="w-4 h-4 text-red-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                  </svg>
-                  <span className="text-gray-600 ml-3">4 Reviews</span>
+                  {Array(rating)
+                    .fill(rating)
+                    .map((_, i) => (
+                      <svg
+                        fill="currentColor"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-4 h-4 text-red-500"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                      </svg>
+                    ))}
+                  {rating < 5 && (
+                    <Fragment>
+                      {Array(5 - rating)
+                        .fill(5 - rating)
+                        .map((_, i) => (
+                          <svg
+                            fill="none"
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            className="w-4 h-4 text-red-500"
+                            viewBox="0 0 24 24"
+                          >
+                            <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
+                          </svg>
+                        ))}
+                    </Fragment>
+                  )}
+                  <span className="text-gray-600 ml-3">2 Reviews</span>
                 </span>
                 <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200">
                   <a className="text-gray-500">
@@ -149,14 +165,14 @@ const DetailProduct = () => {
                     currency="IDR"
                   />
                 </span>
-                <button
+                <Button
                   onClick={() => {
                     dispatch(addToBasket(products?.data as Basket));
                   }}
                   className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded"
                 >
                   Add to cart
-                </button>
+                </Button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
