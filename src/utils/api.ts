2d9d43d1e32f71097d6cceea9ef88ed4776e01f7
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2023-01-09 16:50:29 +0700

    add ngrok skip warning headers

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -6,7 +6,8 @@ export const headers = {
   Accept: "application/json",
   "Content-Type": "application/json",
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS"
+  "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  "ngrok-skip-browser-warning": "69420",
 };
 
 let bearerToken: string = "";
